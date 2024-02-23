4b557d72f32f31bc554b82682ef0d5245c850271
Oussama-gharbi <124163293+Oussama-gharbi@users.noreply.github.com>
2024-02-23 23:29:10 +0100

    Update DevopsProjectSamplesApplication.java

--- src/main/java/fr/mossaab/devopsprojectsamples/DevopsProjectSamplesApplication.java
+++ src/main/java/fr/mossaab/devopsprojectsamples/DevopsProjectSamplesApplication.java
@@ -16,7 +16,7 @@ public class DevopsProjectSamplesApplication {
     }
 
     @GetMapping
-    public String sayHello(){ return "Hello from develop branch !";}
+    public String sayHello(){ return "Hello from new branch !";}
 
 
 }
