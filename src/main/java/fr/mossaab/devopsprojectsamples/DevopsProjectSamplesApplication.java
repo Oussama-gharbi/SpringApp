5ae604ba297e358e2fdc498df381d13993a25241
Oussama Gharbi <oussama.gharbi@enit.utm.tn>
2024-02-24 14:10:05 +0100

    changes from feature 1

--- src/main/java/fr/mossaab/devopsprojectsamples/DevopsProjectSamplesApplication.java
+++ src/main/java/fr/mossaab/devopsprojectsamples/DevopsProjectSamplesApplication.java
@@ -16,7 +16,7 @@ public class DevopsProjectSamplesApplication {
     }
 
     @GetMapping
-    public String sayHello(){ return "Hello from new feat  branch :-) !";}
+    public String sayHello(){ return "Hello from new feature  branch :-) !";}
 
 
 }
